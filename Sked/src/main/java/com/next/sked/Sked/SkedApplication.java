3a6a0cc0e5a3ddf6955dc025f90f3b73148fd69c
Zelialdo Sueldo da Silva Lima <71799490+zelialdo@users.noreply.github.com>
2022-11-22 14:19:40 -0300

    a

---
+++ Sked/src/main/java/com/next/sked/Sked/SkedApplication.java
@@ -0,0 +1,13 @@
+package com.next.sked.Sked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkedApplication.class, args);
+	}
+
+}
