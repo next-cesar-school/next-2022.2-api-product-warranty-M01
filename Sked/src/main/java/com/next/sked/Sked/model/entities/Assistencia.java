0f49697b8228738071f08632d032a04bd9dcd4eb
claracoelh <aninhacoelho99@gmail.com>
2022-11-23 11:03:27 -0300

    add @Table

---
+++ Sked/src/main/java/com/next/sked/Sked/model/entities/Assistencia.java
@@ -0,0 +1,25 @@
+package com.next.sked.Sked.model.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class Assistencia {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nome;
+
+}
