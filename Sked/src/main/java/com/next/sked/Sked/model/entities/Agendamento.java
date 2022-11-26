d394c09cb004341a77b76ef45f18312974714e2f
Zelialdo Sueldo da Silva Lima <71799490+zelialdo@users.noreply.github.com>
2022-11-26 10:41:04 -0300

    Update Agendamento.java

--- Sked/src/main/java/com/next/sked/Sked/model/entities/Agendamento.java
+++ Sked/src/main/java/com/next/sked/Sked/model/entities/Agendamento.java
@@ -19,7 +19,7 @@ import lombok.NoArgsConstructor;
 public class Agendamento {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String nomeDoCliente;
 	private String dataHora;
