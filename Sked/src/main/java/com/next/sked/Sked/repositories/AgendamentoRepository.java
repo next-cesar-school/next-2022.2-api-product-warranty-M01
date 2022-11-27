1ab3a5649a10445731929c12d0b3a825bebb175c
Zelialdo Sueldo da Silva Lima <71799490+zelialdo@users.noreply.github.com>
2022-11-27 16:23:20 -0300

    Update AgendamentoRepository.java

--- Sked/src/main/java/com/next/sked/Sked/repositories/AgendamentoRepository.java
+++ Sked/src/main/java/com/next/sked/Sked/repositories/AgendamentoRepository.java
@@ -1,13 +1,16 @@
 package com.next.sked.Sked.repositories;
 
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import java.util.List;
 
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.next.sked.Sked.model.entities.Agendamento;
 
 @Repository
 public interface AgendamentoRepository extends PagingAndSortingRepository<Agendamento, Integer> {
+	
+	public List<Agendamento> findByDataHoraContainingIgnoreCase(String data);
     
 }
