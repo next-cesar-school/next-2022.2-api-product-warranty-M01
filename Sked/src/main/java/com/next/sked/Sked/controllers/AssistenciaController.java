42e7518ab13895465049339ff1b0753425e126cd
matheusmigge <matheusmigge@gmail.com>
2022-11-30 14:20:25 -0300

    Adicionando a função de deletar Assitência cadastrada

--- Sked/src/main/java/com/next/sked/Sked/controllers/AssistenciaController.java
+++ Sked/src/main/java/com/next/sked/Sked/controllers/AssistenciaController.java
@@ -7,7 +7,9 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -40,5 +42,16 @@ public class AssistenciaController {
     	return assistencia;
     }
 
+    @DeleteMapping("/{assistenciaId}")
+  public ResponseEntity<Void> desmarcar(@PathVariable Integer assistenciaId){
+	  if (!assistenciaRepository.existsById(assistenciaId)) {
+		  return ResponseEntity.notFound().build();	
+	  }
+	  
+	  assistenciaRepository.deleteById(assistenciaId);
+	  
+	  return ResponseEntity.noContent().build();
+	  
+  }
     
 }
